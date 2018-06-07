456e7c72b89677f817c02546c4855dac3141c2ce
MasterUNG <phrombutr@gmail.com>
2018-06-08 06:13:02 +0700

    update

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -8,6 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ChildComponent implements OnInit {
 
   @Input() receiveFromParrent: string;
+  @Input() receiveNumber: number;
 
   constructor() { }
 
